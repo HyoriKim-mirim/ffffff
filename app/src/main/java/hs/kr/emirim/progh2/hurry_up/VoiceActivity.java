01a7622475688ad5f4d2bef00e5f2a28660c999d
Hyori Kim <s2010@e-mirim.hs.kr>
2020-09-06 13:21:35 +0900

    안드로이드...기능...

---
+++ app/src/main/java/hs/kr/emirim/progh2/hurry_up/VoiceActivity.java
@@ -0,0 +1,14 @@
+package hs.kr.emirim.progh2.hurry_up;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice);
+    }
+}
